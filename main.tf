fdc7aa7e8db08bf86033b0aab95e71f0da8d7178
llakshman <lakshmanlukalapu1729@gmail.com>
2021-08-16 21:16:42 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "local_file" "sample1" {
+filename = var.filename
+content=var.content
+file_permission=var.filepermission
+}
+
+
---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable filename {
+type=string
+default="pets.txt"
+}
+
+variable content{
+type=string
+default="I love india"
+}
+variable filepermission {
+type =string
+default="0700"
+}
